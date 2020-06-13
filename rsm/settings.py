a088179e87e82cdb57a507ea799f19140fceddcd
Ramadhan Marijani <ramadhanwakil1@gmail.com>
2020-06-13 20:37:54 +0300

    cpanel.yml

--- rsm/settings.py
+++ rsm/settings.py
@@ -83,11 +83,14 @@ WSGI_APPLICATION = 'rsm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'burhanis_result_system',
+        'USER': 'burhanis_burhanisdb',
+        'PASSWORD': 'burhanis112233445566778899',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
