abcd28074699044c88ca2c424fbf627a6e42cdff
Ramadhan Marijani <ramadhanwakil19@gmail.com>
2020-09-05 14:32:21 +0300

    db changes to sqlt

--- rsm/settings.py
+++ rsm/settings.py
@@ -83,14 +83,11 @@ WSGI_APPLICATION = 'rsm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'burhanis_result_system',
-        'USER': 'burhanis_burhanisdb',
-        'PASSWORD': 'burhanis112233445566778899',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
